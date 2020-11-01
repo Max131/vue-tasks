21c972dd745c8d5054e3bc6c9b1beefaf9e685a7
Max131 <max_cable@hotmail.com>
2020-11-01 12:44:56 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vue Tasks
+
+**Vue Tasks** Is a simple ToDo app writen in Vue.js as excersice and learning. You can create, edit and delete.
+
+ToDo:
+- Add local storage for persisten data
+- Made it a PWA
+
+Made it with:
+- [Vue.js](https://vuejs.org)
+- [Bulma](https://bulma.io)
---
+++ app.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		message: "Task manager",
+		task: '',
+		tasks: [
+			{name: "ToDO", state: false},
+			{name: "TaDA", state: false},
+			{name: "TeDE", state: true},
+			{name: "TuDU", state: false}
+		]
+	},
+	methods: {
+		addTask(){
+			if (this.task.trim()!==''){
+				this.tasks.push({name: this.task, state: false});
+				this.task = '';
+			}
+			else{
+				this.task = '';
+			}
+		},
+		taskEdit(key){
+			console.log('Editing task ' + key);
+		},
+		removeTask(key){
+			this.tasks.splice(key, 1);
+		},
+		toggleTask(key){
+			this.tasks[key].state = !this.tasks[key].state ;
+		}
+	}
+})
